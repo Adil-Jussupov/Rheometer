6cddaa96e4f7b7c1199bf30b68e33c4020152b48
Adil-Jussupov <129574314+Adil-Jussupov@users.noreply.github.com>
2024-08-29 11:57:30 -0700

    Control motor speed, direction via Serial input

--- Firmware/StepmotorControl/StepmotorControl.ino
+++ Firmware/StepmotorControl/StepmotorControl.ino
@@ -1,51 +1,68 @@
-/*   
- *   Basic example code for controlling a stepper without library
- *      
- *   by Dejan, https://howtomechatronics.com
- */
+/*
+   Uno sketch to drive a stepper motor using the AccelStepper library.
+   Works with a ULN-2003 unipolar stepper driver, or a bipolar, constant voltage motor driver
+   such as the L298 or TB6612, or a step/direction constant current driver like the a4988.
+// Shows how to run AccelStepper in the simplest,
+// fixed speed mode with no accelerations using runSpeed().
+// Mike McCauley (mikem@airspayce.com)
+// Copyright (C) 2009 Mike McCauley
+12/26/21  Modified to use various drivers vice default.   --jkl
+*/
 
-// defines pins
-#define stepPin D4
-#define dirPin D5
+// Include the AccelStepper Library
+#include <AccelStepper.h>
 
-long prevtime = 0;
-bool stepFlag = HIGH;
-bool dirFlag = HIGH;
-long prevTime = 0;
-int stepDelay = (1250 * 6) / 1.6;  // initial delay at 1 RPM spinRate
-int revolutions = 0;
-float spinRate = 0.100;  // in RPM
+// Motor Connections (constant current, step/direction bipolar motor driver)
+const int dirPin = D5;
+const int stepPin = D4;
+
+// Speed control variables
+int spinRate = 1;  // in RPM
+String inputString = "";
+bool stateUpdate = true;
+int steps = 0;
+
+// Creates an instance - Pick the version you want to use and un-comment it. That's the only required change.
+AccelStepper myStepper(AccelStepper::DRIVER, stepPin, dirPin);  // works for a4988 (Bipolar, constant current, step/direction driver)
 
 void setup() {
   Serial.begin(9600);
-  // Sets the two pins as Outputs
-  pinMode(stepPin, OUTPUT);
-  pinMode(dirPin, OUTPUT);
-  digitalWrite(dirPin, dirFlag);  // Enables the motor to move in a particular direction
-  prevTime = millis();
-  stepDelay = stepDelay / spinRate;
+  myStepper.setMaxSpeed(100000);  // this limits the value of setSpeed(). Raise it if you like.
+  myStepper.setSpeed(500);        // runSpeed() will run the motor at this speed - set it to whatever you like.
 }
+
 void loop() {
+  myStepper.runSpeed();  // This will run the motor forever.
 
-  // Makes 200 pulses for making one full cycle rotation
-  if (revolutions >= 12800){
-    Serial.print("SpinRate: "); Serial.print(spinRate);
-    Serial.print(" RPM |  Time: "); Serial.print(millis() - prevTime);
-    Serial.print(" ms  |  Delay: "); Serial.print(stepDelay); Serial.print("\n--------------------------------------------------------------\n");
-
-    revolutions = 0;
-    //dirFlag = !dirFlag; // change direction every revolution
-    //digitalWrite(dirPin, dirFlag);
-    //digitalWrite(stepPin, true); // release coils in the motor
-    delay(3000);
-
-    prevTime = millis();
+  if (stateUpdate) {
+    Serial.print("SpinRate: ");
+    Serial.print(spinRate);
+    Serial.print(" Units |  Steps: ");
+    Serial.print(steps);
+    Serial.print("\n--------------------------------------------\n");
+    stateUpdate = false;
   }
-  if ((micros() - prevtime) > stepDelay) {
-    digitalWrite(stepPin, stepFlag);
-    prevtime = micros();
-    stepFlag = !stepFlag;
-    ++revolutions;
-  }
+}
 
+void serialEventRun() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag so the main loop can
+    // do something about it:
+    if (inChar == '\n') {
+      if (inputString == "0") myStepper.stop();
+      if (inputString.startsWith("d")) {
+        steps = inputString.toInt();
+        myStepper.move(steps);
+      } else {
+        spinRate = inputString.toInt();
+        myStepper.setSpeed(spinRate);
+      }
+      stateUpdate = true;
+      inputString = "";
+    }
+  }
 }
